f31dd63a7c28ee9f767a523d97d2ac19003071dd
Andy Wickert <andy@northernwidget.com>
2015-01-19 20:59:19 +0100

    Place PrattAiry here so I have a version-tracked record of its existence

--- prattairy.py
+++ prattairy.py
@@ -1,6 +1,14 @@
 from __future__ import division # No automatic floor division
 from base import *
 
+"""
+This code is here as a vestige of a former time.
+It is really just a whole lot of architecture to solve a trivial equation.
+It was somewhat useful to figure out how to build classes way back when...
+This will be its final update before it is deleted sometime before the 1.0
+release, or so I plan.
+-- ADW, 19 January 2015
+"""
 
 class PrattAiry(Isostasy):
   def initialize(self, filename):
