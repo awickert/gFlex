e27872602d7c8edbb68b8fb248e6c7b661dbe427
Andy Wickert <andy@northernwidget.com>
2015-01-22 17:54:10 +0100

    Keep a record of my ongoing testing

--- tests/runtime_experiment_1D.py
+++ tests/runtime_experiment_1D.py
@@ -18,7 +18,7 @@ import time
 from matplotlib import pyplot as plt
 
 # This code is for 2D flexural isostasy (and instantiate)
-obj = gflex.F2D()
+obj = gflex.F1D()
 obj.set_value('Quiet', True)
 # Always use the van Wees and Cloetingh (1994) solution type.
 # It is the best.
@@ -55,50 +55,43 @@ for l in [100E3, 200E3, 500E3, 1000E3]:
   for Te in [25000]:
     Te0 = Te
     #for bc in ["0Slope0Shear", 'Dirichlet0', 'Mirror', '0Slope0Shear']:
-    #for bc in ['Periodic']:
-    for bc in ['NoOutsideLoads']:
-      #ne = []
-      #nle = []
-      #st = []
-      #nelements = .append([])
-      #nloadelements = []
-      #solvetime = []
+    for bc in ['Periodic']:
+    #for bc in ['NoOutsideLoads']:
       #for method in ['SAS', 'SAS_NG', 'FD']:
       #for method in ['FD']:
-      for method in ['SAS_NG']:
-        #for dx in [100, 500, 1000, 2000, 2500, 5000, 10000, 20000, 25000, 50000]:
+      for method in ['FD']:
+        for dx in [100, 500, 1000, 2000, 2500, 5000, 10000, 20000, 25000, 50000]:
         #for dx in [5000, 10000, 25000, 40000, 50000]: #1000, 2000, 2500, 4000,
-        for dx in [10000, 25000, 40000, 50000]:
+        #for dx in [10000, 25000, 40000, 50000]:
         #for dx in [10000]: #, 20000, 25000, 40000, 50000]: #1000, 2000, 2500, 4000, 5000, 10000, 
 
             obj.set_value('Method', method)
             # Grid size and spacing
             # SIZE -- MAINTAIN BLOCKS INSIDE, TOO -- START WORK HERE
-            obj.set_value('GridSpacing_x', dx)
-            obj.set_value('GridSpacing_y', dx)
-            
             
             # Make the array
             if L % dx != 0 or l % dx !=0:
               pass
             else:
               print dx 
-              q = np.zeros((L/dx, L/dx))
-              q[(L/2-l/2)/dx : (L/2+l/2)/dx+.0001, (L/2-l/2)/dx : (L/2+l/2)/dx+.0001] = q_load
+              q0 = np.zeros((L/dx))
+              q0[(L/2-l/2)/dx : (L/2+l/2)/dx+.0001] = q_load
               #obj.set_value('Loads', q)
 
-              # SAS_NG
-              x = np.linspace(dx/2, L-dx/2, q.shape[1])
-              y = np.linspace(dx/2, L-dx/2, q.shape[0])
-              X,Y = np.meshgrid(x,y)
-              
               #q0 = np.vstack((np.reshape(X,-1), np.reshape(Y,-1), np.reshape(q,-1))).transpose()
               
-              obj.set_value('x', np.reshape(X,-1))
-              obj.set_value('y', np.reshape(Y,-1))
-              obj.set_value('Loads_force', np.reshape(q,-1))
+              if method == 'SAS_NG':
+                x = np.linspace(dx/2, L-dx/2, len(q0))
+                obj.set_value('x', x)
+                obj.set_value('Loads_force', np.reshape(q0,-1))
+              else:
+                # Getters and setters only exist because of old CSDMS IRF
+                # interface anyway :)
+                obj.qs = q0
+                obj.dx = dx
               
               """
+              # VARIABLE TE OPTION
               Te *= np.ones(q.shape)
               
               # Make sinusoid
@@ -135,7 +128,7 @@ for l in [100E3, 200E3, 500E3, 1000E3]:
               nloadelements.append(np.sum(q > 0))
               solvetime.append(obj.time_to_solve)
               
-              print len(obj.q)
+              print len(obj.q0)
               
               print 'Time to solve [s]:', obj.get_value('SolverTime')
               
@@ -201,6 +194,6 @@ outarray = np.vstack((nelements, nloadelements, nelements*nloadelements, solveti
 
 
 #np.savetxt('benchmark/SAS.csv', outarray, delimiter=',',)
-np.savetxt('benchmark/SAS_NG.csv', outarray, delimiter=',',)
+#np.savetxt('benchmark/SAS_NG.csv', outarray, delimiter=',',)
 
 
