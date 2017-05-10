f3019c43119e1b50369739959707e6a34ef0eaf3
Andy Wickert <andy@northernwidget.com>
2017-05-10 18:12:38 +0200

    Version 1.0.0

--- gflex/_version.py
+++ gflex/_version.py
@@ -3,5 +3,5 @@
 # The best way to do this is following @Zooko's answer at:
 # http://stackoverflow.com/questions/458550/standard-way-to-embed-version-into-python-package
 
-__version__ = '1.0'
+__version__ = '1.0.0'
 
