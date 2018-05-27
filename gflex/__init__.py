3dcdb14c09afb444f96ead1f8cfac3a3d744a688
Andy Wickert <andy@northernwidget.com>
2018-05-27 12:34:14 -0500

    import all from all

--- gflex/__init__.py
+++ gflex/__init__.py
@@ -3,4 +3,8 @@ import os
 # If run from up one level
 sys.path.append( os.path.dirname(os.path.realpath(__file__)) )
 os.chdir( os.path.dirname(os.path.realpath(__file__)) )
+print(os.getcwd())
 from gflex import *
+from f1d import *
+from f2d import *
+from base import *
