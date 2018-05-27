1e0a8c8e778f288e98f2fc05c1999f6fed9c3882
Andy Wickert <andy@northernwidget.com>
2018-05-27 13:07:34 -0500

    remove extra debug commands

--- gflex/__init__.py
+++ gflex/__init__.py
@@ -1,9 +1,6 @@
 import sys
 import os
-# If run from up one level
 sys.path.append( os.path.dirname(os.path.realpath(__file__)) )
-os.chdir( os.path.dirname(os.path.realpath(__file__)) )
-print(os.getcwd())
 from gflex import *
 from f1d import *
 from f2d import *
