586bd38c5e750a77c80d55ac211d20d0b94f3861
Andy Wickert <andy@northernwidget.com>
2021-01-15 20:39:37 -0600

    Update copyright

--- gflex/f1d.py
+++ gflex/f1d.py
@@ -1,7 +1,7 @@
 """
 This file is part of gFlex.
 gFlex computes lithospheric flexural isostasy with heterogeneous rigidity
-Copyright (C) 2010-2018 Andrew D. Wickert
+Copyright (C) 2010-2020 Andrew D. Wickert
 
 gFlex is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
