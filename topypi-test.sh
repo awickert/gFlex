ea9b7edd728507c3625d9794e0ebd8f4f8d9aede
Andy Wickert <andy@northernwidget.com>
2021-06-26 11:01:48 -0500

    PyPI now reads md

--- topypi-test.sh
+++ topypi-test.sh
@@ -1,6 +1,4 @@
 #! /usr/bin/env sh
 
-python3 register.py
-rm README.txt
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 #python setup.py sdist upload -r pypitest
--- topypi.sh
+++ topypi.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env sh
 
-#python register.py
-#rm README.txt
 python3 setup.py sdist upload -r pypi
