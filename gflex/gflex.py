084526933ced70efddd6251177b658f96554433c
mcflugen <mcflugen@gmail.com>
2023-12-01 14:42:27 -0700

    use argparse for the cli

--- gflex/gflex.py
+++ gflex/gflex.py
@@ -1,6 +1,33 @@
 #! /usr/bin/env python
-
 """
+Multiple methods to solve elastic plate flexure, designed for applications
+to Earth's lithosphere.
+
+
+To generate an input file, please see the examples in the "input"
+directory of this install.
+
+To run in a Python script or shell, follow this general pattern:")
+
+```
+import gflex
+flex = gflex.F1D()
+flex.method = ...
+# ...more variable setting...
+# see the 'input' directory for examples
+```
+"""
+
+import argparse
+import sys
+
+from gflex.base import WhichModel
+from gflex.f1d import F1D
+from gflex.f2d import F2D
+
+from ._version import __version__
+
+LICENSE = """
 This file is part of gFlex.
 gFlex computes lithospheric flexural isostasy with heterogeneous rigidity
 Copyright (C) 2010-2018 Andrew D. Wickert
@@ -19,93 +46,39 @@ You should have received a copy of the GNU General Public License
 along with gFlex.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import sys
-
-from gflex.base import WhichModel
-from gflex.f1d import F1D
-from gflex.f2d import F2D
-
-from ._version import __version__
-
-
-def welcome():
-    print("")
-    print("**************************" + "*" * len(__version__))
-    print("*** WELCOME to gFlex v" + __version__ + " ***")
-    print("**************************" + "*" * len(__version__))
-    print("")
-
-
-def displayUsage():
-    print("Open-source licensed under GNU GPL v3")
-    print("")
-    print("Usage:")
-    print("gflex <<path_to_configuration_file>>  # TO RUN STANDALONE")
-    print("gflex -h  *OR*  gflex --help          # DISPLAY ADDITIONAL HELP")
-    print("gflex -v  *OR*  gflex --version       # DISPLAY VERSION NUMBER")
-    print("import gflex                          # WITHIN PYTHON SHELL OR SCRIPT")
-    print("")
-
-
-def furtherHelp():
-    print("")
-    print("ADDITIONAL HELP:")
-    print("--------------- ")
-    print("")
-    print('To generate an input file, please see the examples in the "input"')
-    print("directory of this install.")
-    print("")
-    print("To run in a Python script or shell, follow this general pattern:")
-    print("import gflex")
-    print("flex = gflex.F1D()")
-    print("flex.method = ...")
-    print("# ...more variable setting...")
-    print("# see the 'input' directory for examples")
-    print("")
-
 
 def main():
-    # Choose how to instantiate
-    if len(sys.argv) == 2:
-        if sys.argv[1] == "--help" or sys.argv[1] == "-h":
-            welcome()
-            displayUsage()
-            furtherHelp()
-            return
-        if sys.argv[1] == "--version" or sys.argv[1] == "-v":
-            print("gFlex v" + __version__)
-            return
-        else:
-            # Looks like it wants to be an configuration file!
-            filename = sys.argv[1]  # it works for usage (1) and (2)
-            obj = WhichModel(filename)
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument("filename", nargs=1, help="gflex configuration file.")
+    parser.add_argument("--version", action="version", version=f"gflex {__version__}")
+    parser.add_argument(
+        "--verbose", action="store_true", help="print debugging information."
+    )
+    parser.add_argument("--silent", action="store_true", help="print minimal output.")
 
-    elif len(sys.argv) == 1:
-        welcome()
-        displayUsage()
-        print("")
-        sys.exit()
-    else:
-        welcome()
-        print(">>>> ERROR: Too many input parameters provided; exiting. <<<<")
-        print("")
-        displayUsage()
-        print("")
-        sys.exit()
+    args = parser.parse_args()
+
+    if not args.silent:
+        print(LICENSE, file=sys.stderr)
+
+    obj = WhichModel(args.filename)
+
+    obj.Debug = args.verbose
+    obj.Quiet = args.silent
 
     ########################################
     ## SET MODEL TYPE AND DIMENSIONS HERE ##
     ########################################
 
     if obj.dimension == 1:
-        obj = F1D(filename)
+        obj = F1D(args.filename)
     elif obj.dimension == 2:
-        obj = F2D(filename)
+        obj = F2D(args.filename)
 
-    obj.initialize(filename)
-
-    if obj.Debug:
-        print("Command line:", sys.argv)
+    obj.initialize(args.filename)
 
     ############################################
     ##       SET MODEL PARAMETERS HERE        ##
